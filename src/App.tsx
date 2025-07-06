a17f86bf2107bae999a4aa3db1ea9f638189a942
Nidhi Verma <nidhi.verma@zealholdings.in>
2025-07-06 17:11:25 +0530

    reove unused varibale

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { url } from './constant';
 import Answers from './component/answers';
-import { Loader, Trash } from 'lucide-react';
+import { Trash } from 'lucide-react';
 import AnimationLoader from './component/AnimationLoader';
 
 function App() {
